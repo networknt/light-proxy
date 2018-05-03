240da61874527f99bbb055c8846421d74be5dcc5
Steve Hu <stevehu@gmail.com>
2018-05-02 22:23:36 -0400

    fixes #20 switch tableau auth handler to simple one without cache

--- src/main/java/com/networknt/proxy/tableau/TableauConfig.java
+++ src/main/java/com/networknt/proxy/tableau/TableauConfig.java
@@ -1,7 +1,7 @@
 package com.networknt.proxy.tableau;
 
 /**
- * Config class for TableauAuthHandler
+ * Config class for TableauCacheAuthHandler
  *
  * @author Steve Hu
  */
@@ -9,11 +9,7 @@ public class TableauConfig {
     boolean enabled;
     String serverUrl;
     String serverPath;
-    long tokenRenewBeforeExpired;
-    long expiredRefreshRetryDelay;
-    long earlyRefreshRetryDelay;
     String tableauUsername;
-    String tableauContentUrl;
 
     public TableauConfig() {
 
@@ -43,43 +39,11 @@ public class TableauConfig {
         this.serverPath = serverPath;
     }
 
-    public long getTokenRenewBeforeExpired() {
-        return tokenRenewBeforeExpired;
-    }
-
-    public void setTokenRenewBeforeExpired(long tokenRenewBeforeExpired) {
-        this.tokenRenewBeforeExpired = tokenRenewBeforeExpired;
-    }
-
-    public long getExpiredRefreshRetryDelay() {
-        return expiredRefreshRetryDelay;
-    }
-
-    public void setExpiredRefreshRetryDelay(long expiredRefreshRetryDelay) {
-        this.expiredRefreshRetryDelay = expiredRefreshRetryDelay;
-    }
-
-    public long getEarlyRefreshRetryDelay() {
-        return earlyRefreshRetryDelay;
-    }
-
-    public void setEarlyRefreshRetryDelay(long earlyRefreshRetryDelay) {
-        this.earlyRefreshRetryDelay = earlyRefreshRetryDelay;
-    }
-
     public String getTableauUsername() {
         return tableauUsername;
     }
 
     public void setTableauUsername(String tableauUsername) {
         this.tableauUsername = tableauUsername;
-    }
-
-    public String getTableauContentUrl() {
-        return tableauContentUrl;
-    }
-
-    public void setTableauContentUrl(String tableauContentUrl) {
-        this.tableauContentUrl = tableauContentUrl;
     }
 }
--- src/main/java/com/networknt/proxy/tableau/TableauTokenHelper.java
+++
@@ -1,4 +0,0 @@
-package com.networknt.proxy.tableau;
-
-public class TableauTokenHelper {
-}
