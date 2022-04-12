79f1dc19acddbcc9e30b8169c31d080a85681972
Steve Hu <stevehu@gmail.com>
2022-04-11 22:13:28 -0400

    fixes #84 move the tableau authentication handler to the light-4j ingress-proxy module (#85)

---
+++ src/main/java/com/networknt/proxy/Dummy.java
@@ -0,0 +1,16 @@
+package com.networknt.proxy;
+
+/**
+ * This is a dummy class that to force the fat jar to be released.
+ *
+ * @author Steve Hu
+ */
+public class Dummy {
+    /**
+     * @param name Name
+     */
+    public void sayHello(String name) {
+        ;
+    }
+
+}
